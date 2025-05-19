cf07e452e8205867d7a8789940290d6e338927c5
SergeyChitsvarin <Sergeychits20@gmail.com>
2025-05-19 14:31:24 -0600

    Footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ export default function Footer() {
     <div className="bg-primary text-white p-3 text-center align-items-bottom">
         <div className="container d-flex align-items-center justify-content-start">
             <h1> Contact Us</h1>
-            <ul>
-                <li>Tel: (123)-456-7890</li>
-                <li>Email: abc@Mail.com</li>
-                <li>Address: 123 Moose St, A1B 2C3</li>
+            <ul className="list-unstyled p-2">
+                <li>☎ (123)-456-7890</li>
+                <li>✉ abc@Mail.com</li>
+                <li>📍 123 Moose St</li>
             </ul>
         </div>
     </div>
