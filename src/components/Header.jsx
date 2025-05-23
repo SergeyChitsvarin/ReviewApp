4a0705c7adcfc56c90e310bb58f232785a11115b
SergeyChitsvarin <Sergeychits20@gmail.com>
2025-05-22 22:07:09 -0600

    Header includes log-out button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,17 +19,17 @@ export default function Header() {
     // Header component showing logo and text allowing user to go to home page
   return (
     <div className="navbar sticky-top bg-body-tertiary text-white text-center">
-        <div className="container d-flex align-items-center justify-content-center">
+        <div className="container-fluid d-flex align-items-center ">
             <img
                 src="/logoReviewApp.jpg"
                 alt="Logo"
-                className="m-2 "
+                className="m-2 justify-content-center"
                 style={{ height: "50px", width: "50px", cursor: "pointer" }}
                 onClick={goToMainPage}
             />
-            <h1 className="m-0">Review Guru</h1>
+            <h1 className="m-0 justify-content-center">Review Guru</h1>
+            <button onClick={HandleLogOut} className="justify-content-right btn btn-secondary">Log Out</button>
         </div>
-        <button onClick={HandleLogOut} className="justify-content-right btn btn-secondary">Log Out</button>
     </div>
   );
 }
