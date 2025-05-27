cc84f729a77f0753ee2d110ebcd39dc809f1dd03
SergeyChitsvarin <Sergeychits20@gmail.com>
2025-05-26 21:13:54 -0600

    Added Markers to major Canadian cities

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,22 +1,76 @@
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
+export default function CanadianMap() {
 
-export default function CanadianMap()
-{
-    const centre = [53.334242362310036, -107.33698365737052];
+  const centre = [53.334242362310036, -107.33698365737052];
+  const cityMarkers = [
+    {
+      geocode: { lat: 51.067349, lng: -114.073226 }, // 52.143223, -106.656894
+      popUp: "Calgary"
+    },
+    {
+      geocode: { lat: 53.550197, lng: -113.491190 },
+      popUp: "Edmonton"
+    },
+    {
+      geocode: { lat: 49.250249, lng: -123.162260 },
+      popUp: "Vancouver"
+    },
+    {
+      geocode: { lat: 52.143223, lng: -106.656894 },
+      popUp: "Saskatoon"
+    },
+    {
+      geocode: { lat: 50.452265, lng: -104.602649 }, 
+      popUp: "Regina"
+    },
+    {
+      geocode: { lat: 49.904130, lng: -97.173133 },
+      popUp: "Winnipeg"
+    },
+    {
+      geocode: { lat: 43.679809, lng: -79.366570 },
+      popUp: "Toronto"
+    },
+    {
+      geocode: { lat: 45.438684, lng: -75.727755 },
+      popUp: "Ottawa"
+    },
+    {
+      geocode: { lat: 45.558673, lng: -73.601452 },
+      popUp: "Montreal"
+    },
+        {
+      geocode: { lat: 48.421473, lng: -123.381733 },
+      popUp: "Victoria"
+    },
+    {
+      geocode: { lat: 45.438684, lng: -75.727755 },
+      popUp: "Ottawa"
+    },
+    {
+      geocode: { lat: 44.672155, lng: -63.600806 },
+      popUp: "Halifax"
+    }
+  ];
 
-    // Candadian map component using leaflet and maptiler to create the map
-    return(
-        <MapContainer 
-                center={centre} 
-                zoom={4} 
-                style={{ height: '100vh'}}>
-            <TileLayer 
-               url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=yU4ZkrzV8EbltZ688TjL"
-               attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-            />
-        </MapContainer>
+  return (
+    <MapContainer 
+      center={centre} 
+      zoom={4} 
+      style={{ height: '100vh', width: '100%' }}
+    >
+      <TileLayer 
+        url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=yU4ZkrzV8EbltZ688TjL"
+        attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+      />
 
-    )    
+      {cityMarkers.map((marker, index) => (
+        <Marker key={index} position={[marker.geocode.lat, marker.geocode.lng]}>
+          <Popup>{marker.popUp}</Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
 }
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,25 +1,19 @@
-import { useNavigate } from "react-router-dom"
 import Header from "../components/Header";
 import Footer from "../components/Footer"
 import CanadianMap from "../components/Map";
 
 export default function MainPage() {
-  const navigate = useNavigate();
 
-  // User can look at the map of Canada and choose by province, what reviews they would like to see
+  // User can look at the map of Canada and choose reviews based on the province.
   return (
           <>
           <Header />
-
-          <div className="container border border-tertiary">
-            
-
+          <div className="container border border-tertiary rounded">
             <div class="container">
-              <h3 className=""> Please Select a Canadian Province to browse reviews.</h3>
+              <h3 className="text-center"> Please Select a Canadian city to browse reviews</h3>
               <CanadianMap/>
             </div>
           </div>
-          
           <Footer />
           </>
   )
