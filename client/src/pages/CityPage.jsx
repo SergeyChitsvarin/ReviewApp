6b756ae085ada9dd239934904e97408faca57675
SergeyChitsvarin <Sergeychits20@gmail.com>
2025-05-28 22:45:40 -0600

    Header and Footer added to CityPage

--- client/src/pages/CityPage.jsx
+++ client/src/pages/CityPage.jsx
@@ -1,3 +1,5 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import { useParams } from 'react-router-dom';
 
 export default function CityPage() {
@@ -6,9 +8,15 @@ export default function CityPage() {
     const { cityName } = useParams();
 
   return (
+    <>
+    <Header/>
+    
     <div className="p-2">
       <h1>This is {cityName}</h1>
       {/* fetch <cities/cityName> city data*/}
     </div>
+
+    <Footer/>
+    </>
   );
 }
