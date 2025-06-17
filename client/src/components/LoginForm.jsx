722d04c9f1e51336aced65c9c09b219af8dbd5c4
SergeyChitsvarin <Sergeychits20@gmail.com>
2025-06-16 20:35:21 -0600

    Created Login Form Component

---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,40 @@
+import { useNavigate } from 'react-router-dom'
+import React from 'react'
+
+export default function LoginForm(){
+    // Login form component for login page
+
+    const navigate = useNavigate()
+
+    const handleLogInAttempt = () => {
+        //if correct user credentials/ login as guest is checked go to browse log in page
+        navigate('/browseReviews')
+
+    }
+
+    return(
+        <>
+        <form className='px-4 py-3 d-flex flex-column align-items-center justify-content-center'>
+            <div>
+              <h1 className="mb-4 text-center">Login</h1>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="formUser" className="form-label px-1">Username: </label>
+              <input type="email" placeholder='YourEmail@Mail.com' id="formUser" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="formPass" className="form-label px-1">Password:  </label>
+              <input type="password" placeholder='Password' id="formPass" />
+            </div>
+            <div className='mb-3'>
+              <label htmlFor="formGuestLogIn" className='form-label px-1'> log-in as guest: </label>
+              <input type="checkbox" id='formGuestLogIn'/>
+            </div>
+            <button className="btn btn-primary m-3" onClick={handleLogInAttempt}>
+              Log In
+            </button>
+        </form>
+        </>
+    )
+}
+
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,40 +1,15 @@
-import { useNavigate } from 'react-router-dom'
 import Footer from '../components/Footer'
+import LoginForm from '../components/LoginForm'
 
 export default function LoginPage() {
-  const navigate = useNavigate()
-
-  const handleLogInAttempt = () => {
-    //if correct user credentials/ login as guest is checked go to browse log in page
-    navigate('/browseReviews')
-
-  }
+  
 
   return (
     //Web page used for signing in or creating an account to access the rest of the website as a logged in user.
     <>
       <div className="container d-flex flex-column justify-content-center align-items-center min-vh-100">
         <div className="border rounded p-4 shadow">
-          <form className='px-4 py-3 d-flex flex-column align-items-center justify-content-center'>
-            <div>
-              <h1 className="mb-4 text-center">Login</h1>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="formUser" className="form-label px-1">Username: </label>
-              <input type="email" placeholder='YourEmail@Mail.com' id="formUser" />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="formPass" className="form-label px-1">Password:  </label>
-              <input type="password" placeholder='Password' id="formPass" />
-            </div>
-            <div className='mb-3'>
-              <label htmlFor="formGuestLogIn" className='form-label px-1'> log-in as guest: </label>
-              <input type="checkbox" id='formGuestLogIn'/>
-            </div>
-            <button className="btn btn-primary m-3" onClick={handleLogInAttempt}>
-              Log In
-            </button>
-          </form>
+        <LoginForm/>
         </div>
       </div>
       <Footer />
