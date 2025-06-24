bf79859da2bbf2fd827a6ded400bdef98239b641
SergeyChitsvarin <Sergeychits20@gmail.com>
2025-06-23 22:46:37 -0600

    setting up users.js for authentication

--- client/src/components/AuthProvider.jsx
+++ client/src/components/AuthProvider.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import { createContext, useContext, useEffect, useLayoutEffect, useState } from 'react'
+
+
 
 export default function AuthProvider() {
+
+    const AuthContext = createContext(undefined);
+    const useAuth = () =>{
+        const authContext = useContext(AuthContext);
+
+        if (!authContext){
+            throw new Error("useAuth must be used within a AuthProvider");
+        }
+
+        return authContext;
+    };
+    const AuthProvider = ({ children }) => {
+        const [token, setToken] = useState();
+
+        useEffect(() =>{
+            const fetchMe = async () => {
+                try {
+                    const response = await fetch('http://localhost:5000/users');
+                    setToken(response.data.accessToken);
+                    
+                } catch {
+                    setToken(null);    
+                }
+            }
+        })
+    }
+
   return (
-    <div>AuthProvider</div>
+    <></>
   )
 }
