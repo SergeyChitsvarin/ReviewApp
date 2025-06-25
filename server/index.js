43ab6e428913dd9a4e632f683a7edefbdf9e02e7
SergeyChitsvarin <Sergeychits20@gmail.com>
2025-06-24 22:51:10 -0600

    Cleaning up code in the index file

--- server/index.js
+++ server/index.js
@@ -1,17 +1,23 @@
+const PORT = 5000;
 const express = require('express');
 const app = express();
 const cityRouter = require('./routes/cities');
 const userRouter = require('./routes/users');
-const cors = require('cors');
+const authRouter = require('./routes/auth');
 
-app.use(cors());
+const cors = require('cors');
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true                
+}));
+
 app.use(express.json());
 
 app.use("/cities", cityRouter);
-
 app.use('/users', userRouter);
+app.use('/auth', authRouter);
 
-app.listen(5000, () => {
-    console.log("Server started on port 5000!");
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
 
